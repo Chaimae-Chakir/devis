244fd6188380ae47f8778e8c00bc9ab585a45d83
Chaimae Chakir <chaimaechakir24@gmail.com>
2025-05-12 20:55:23 +0100

    refactoring

--- src/main/java/ma/agilisys/devis/utils/Constants.java
+++ src/main/java/ma/agilisys/devis/utils/Constants.java
@@ -3,10 +3,4 @@ package ma.agilisys.devis.utils;
 public final class Constants {
     public static final String DRAFT_STATUS = "Brouillon";
     public static final String APPROVED_STATUS = "Validé";
-    public static final String SUBMITTED_STATUS = "Envoyé";
-    public static final String RECU_STATUS = "Recu";
-    public static final String VERIFIE_STATUS = "Vérifié";
-    public static final String ACCEPTE_STATUS = "Accepté";
-    public static final String REJETE_STATUS = "Rejeté";
-    public static final String PDF_GENERATION_ERROR = "erreur lors de la génération de PDF";
 }
